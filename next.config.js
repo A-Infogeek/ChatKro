0032da1f57e9c1f7b161ef07a76ef3f667041109
A-Infogeek <rishabhraj026@gmail.com>
2023-07-21 01:20:09 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // experimental: {
+    //     appDir: true,
+    //     swcPlugins: [["next-superjson-plugin", {}]]
+    //   }
+    // images: {
+    //     domains: [
+    //     //   'res.cloudinary.com', 
+    //     //   'avatars.githubusercontent.com',
+    //       'lh3.googleusercontent.com'
+    //     ]
+    //   }
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+            port: '',
+            pathname: "**",
+          },
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: "**",
+          },
+          {
+            protocol: 'https',
+            hostname: 'avatars.githubusercontent.com',
+            port: '',
+            pathname: "**",
+          },
+          
+        ],
+      },
+}
 
 module.exports = nextConfig
